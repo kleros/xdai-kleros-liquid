e5b670aef3eb8c663d4c551d4b23c470cf9daed8
fnanni-0 <fnanni@itba.edu.ar>
2021-04-22 01:36:47 -0300

    docs: fix randomaura reference

--- contracts/interfaces/IRandomAuRa.sol
+++ contracts/interfaces/IRandomAuRa.sol
@@ -1,11 +1,11 @@
 pragma solidity ^0.4.24;
 
 
-///@dev See https://blockscout.com/poa/core/address/0x67e90a54AeEA85f21949c645082FE95d77BC1E70/contracts
+///@dev See https://blockscout.com/poa/xdai/address/0x5870b0527DeDB1cFBD9534343Feda1a41Ce47766/contracts
 ///@dev Sokol testnet: https://blockscout.com/poa/sokol/address/0x8f2b78169B0970F11a762e56659Db52B59CBCf1B/contracts
 interface IRandomAuRa {
     function currentSeed() external view returns(uint256);
     function isCommitPhase() external view returns(bool);
     function nextCommitPhaseStartBlock() external view returns(uint256);
-    function collectRoundLength() external view returns(uint256); // 40
+    function collectRoundLength() external view returns(uint256);
 }
